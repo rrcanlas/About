0a83ca807809f2723cccd1c1d524570ea8691bb5
rose <rrbgc0512@gmail.com>
2020-04-14 20:13:56 -0700

    edit package.json

---
+++ build/precache-manifest.4f59421f0be6cd36349e1af933116143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6266a6c770dabdd2b9ffc8492ab00e99",
+    "url": "/Portfolio-v3/static/media/resume.6266a6c7.pdf"
+  },
+  {
+    "revision": "72c584692350ac21b08c",
+    "url": "/Portfolio-v3/static/js/runtime~main.72c58469.js"
+  },
+  {
+    "revision": "12dfa3b85e40444e2227",
+    "url": "/Portfolio-v3/static/js/main.12dfa3b8.chunk.js"
+  },
+  {
+    "revision": "b37c4745bf3a60d725b9",
+    "url": "/Portfolio-v3/static/js/1.b37c4745.chunk.js"
+  },
+  {
+    "revision": "12dfa3b85e40444e2227",
+    "url": "/Portfolio-v3/static/css/main.a6464a8d.chunk.css"
+  },
+  {
+    "revision": "b37c4745bf3a60d725b9",
+    "url": "/Portfolio-v3/static/css/1.55f5a567.chunk.css"
+  },
+  {
+    "revision": "41ece8d8bfbcd09b12efa929db239312",
+    "url": "/Portfolio-v3/index.html"
+  }
+];
