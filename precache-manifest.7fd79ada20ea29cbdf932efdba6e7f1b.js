a20694501e7ddcef724a88e4831792b990f34eec
rose <rrbgc0512@gmail.com>
2020-04-14 20:40:59 -0700

    Updates

---
+++ precache-manifest.7fd79ada20ea29cbdf932efdba6e7f1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6266a6c770dabdd2b9ffc8492ab00e99",
+    "url": "/Portfolio-v3/About/static/media/resume.6266a6c7.pdf"
+  },
+  {
+    "revision": "8dd157c933ada00fc902",
+    "url": "/Portfolio-v3/About/static/js/runtime~main.8dd157c9.js"
+  },
+  {
+    "revision": "cc2a78ff3a3cd4f20d3c",
+    "url": "/Portfolio-v3/About/static/js/main.cc2a78ff.chunk.js"
+  },
+  {
+    "revision": "2eb469d60f40a8e2a8e8",
+    "url": "/Portfolio-v3/About/static/js/1.2eb469d6.chunk.js"
+  },
+  {
+    "revision": "cc2a78ff3a3cd4f20d3c",
+    "url": "/Portfolio-v3/About/static/css/main.a6464a8d.chunk.css"
+  },
+  {
+    "revision": "2eb469d60f40a8e2a8e8",
+    "url": "/Portfolio-v3/About/static/css/1.55f5a567.chunk.css"
+  },
+  {
+    "revision": "2c8a3b6ea28b4cf268f246e760707dbe",
+    "url": "/Portfolio-v3/About/index.html"
+  }
+];
