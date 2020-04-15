eed1ff9f2594ac490569f51ef7f666020e73d663
rose <rrbgc0512@gmail.com>
2020-04-14 23:04:06 -0700

    edit path

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/About/precache-manifest.574a7ba0500bfc1c5f29b261e0042508.js"
+  "/Project/precache-manifest.b65ae9b329c2d47deea01a3fcbccf4ba.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/About/index.html", {
+workbox.routing.registerNavigationRoute("/Project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
