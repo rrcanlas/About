5febbe78ac625f6f7fb1b4a823ffe059e63e93c3
rose <rrbgc0512@gmail.com>
2020-04-15 21:12:12 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Project/precache-manifest.1756341d53c2b0e3ebed7c8a83c962e1.js"
+  "/About/precache-manifest.574a7ba0500bfc1c5f29b261e0042508.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Project/index.html", {
+workbox.routing.registerNavigationRoute("/About/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
