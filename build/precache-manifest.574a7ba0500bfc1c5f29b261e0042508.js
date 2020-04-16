371c1a4006503e306ff235687cdbb2448eaa9c0f
rose <rrbgc0512@gmail.com>
2020-04-15 21:15:03 -0700

    edit path

---
+++ build/precache-manifest.574a7ba0500bfc1c5f29b261e0042508.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6266a6c770dabdd2b9ffc8492ab00e99",
+    "url": "/About/static/media/resume.6266a6c7.pdf"
+  },
+  {
+    "revision": "06239d40c2bfa9a97c76",
+    "url": "/About/static/js/runtime~main.06239d40.js"
+  },
+  {
+    "revision": "374f35f21eebec83f184",
+    "url": "/About/static/js/main.374f35f2.chunk.js"
+  },
+  {
+    "revision": "85ad6d4db1b8faa1c4ba",
+    "url": "/About/static/js/1.85ad6d4d.chunk.js"
+  },
+  {
+    "revision": "374f35f21eebec83f184",
+    "url": "/About/static/css/main.4abc8a4e.chunk.css"
+  },
+  {
+    "revision": "85ad6d4db1b8faa1c4ba",
+    "url": "/About/static/css/1.8bc6f935.chunk.css"
+  },
+  {
+    "revision": "cafb8eef5e0dff7a992f6b38d0c0ed70",
+    "url": "/About/index.html"
+  }
+];
